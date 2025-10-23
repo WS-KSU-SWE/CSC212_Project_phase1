5a04c85307c556a3a034ff4800741b7696002f34
WS-KSU-SWE <ws.ksu.swe@gmail.com>
2025-10-23 17:10:16 +0300

    Linked list implementation completed

---
+++ project_Phase_1_CSC212/src/project_Phase_1_CSC212/Node.java
@@ -0,0 +1,15 @@
+package project_Phase_1_CSC212;
+
+public class Node<T> {
+
+	public T data;
+	
+	public Node<T> next;
+	public Node<T> prevoius;
+	
+	
+	public Node(T data) {
+		this.data = data;
+	}
+	
+}
