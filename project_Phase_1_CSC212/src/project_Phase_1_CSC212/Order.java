967af90a02f61ab7de63acb8d843df462da5a602
WS-KSU-SWE <ws.ksu.swe@gmail.com>
2025-11-01 16:06:23 +0300

    Implemented Order, Date, and Review class. Implementer: Khalid

--- project_Phase_1_CSC212/src/project_Phase_1_CSC212/Order.java
+++ project_Phase_1_CSC212/src/project_Phase_1_CSC212/Order.java
@@ -2,4 +2,71 @@ package project_Phase_1_CSC212;
 
 public class Order {
 
+	private int orderId;
+    private double totalPrice;
+    private Date orderDate;
+    private String status;
+    private Customer customerReference;
+    private DoubleLinkedList<Product> productList;
+
+    public Order(int orderId, double totalPrice, Date orderDate, String status, Customer customerReference) {
+    	
+        this.orderId = orderId;
+        this.totalPrice = totalPrice;
+        this.orderDate = orderDate;
+        this.status = status;
+        this.customerReference = customerReference;
+        this.productList = new DoubleLinkedList<Product>();
+    
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Customer getCustomerReference() {
+        return customerReference;
+    }
+
+    public void setCustomerReference(Customer customerReference) {
+        this.customerReference = customerReference;
+    }
+
+    public DoubleLinkedList<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(DoubleLinkedList<Product> productList) {
+        this.productList = productList;
+    }
+
+	
 }
--- project_Phase_1_CSC212/src/project_Phase_1_CSC212/Review.java
+++ project_Phase_1_CSC212/src/project_Phase_1_CSC212/Review.java
@@ -2,4 +2,38 @@ package project_Phase_1_CSC212;
 
 public class Review {
 
+	private int rating;
+    private String comment;
+    private Customer reviewer;
+
+    public Review(int rating, String comment, Customer reviewer) {
+        this.rating = rating;
+        this.comment = comment;
+        this.reviewer = reviewer;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Customer getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(Customer reviewer) {
+        this.reviewer = reviewer;
+    }
+	
 }
